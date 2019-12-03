c98e3de5e55b92e8e12194a079703942e2fc07fb
Byeonguk Choi mac <cbw0916@mailplug.co.kr>
2019-12-03 10:01:37 +0900

    make docs folder for deploy

---
+++ docs/component---src-pages-index-js-dbfac8e475c449d2cbfb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),c=t.n(o);e.default=function(){return c.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-dbfac8e475c449d2cbfb.js.map
